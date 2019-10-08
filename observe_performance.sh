fe4b338856aee699d33c5a14fc082efa8a4bc76d
johSchm <johSchm@protonmail.com>
2019-10-08 16:43:42 +0200

    tested tensor board

---
+++ observe_performance.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+# -------------------------------------------
+# author:     Johann Schmidt
+# date:       October 2019
+# -------------------------------------------
+
+echo "Starting Tensor Board in Browser ..."
+tensorboard --logdir=logs/
