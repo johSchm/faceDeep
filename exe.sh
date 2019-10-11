5eee5fb4ef43fafead4ccc55dc3c9b5f6b025a72
johSchm <johSchm@protonmail.com>
2019-10-11 11:14:11 +0200

    Adapted exe.sh

--- exe.sh
+++ exe.sh
@@ -5,10 +5,19 @@
 # date:       October 2019
 # -------------------------------------------
 
-#echo "Starting learner ..."
-#~/git/faceDeep/venv/bin/python3 ~/git/faceDeep/src/faceDeep.py
 
-echo "Starting Tensor Board in Browser ..."
-/usr/bin/tensorboard --logdir=logs/ &
-sleep 2s
-chromium http://localhost:6006/ -incognito
+START_LEARNER=False
+TENSORBOARD=True
+
+
+if [ $START_LEARNER == True ] ; then
+  echo "Starting learner ..."
+  ~/git/faceDeep/venv/bin/python3 ~/git/faceDeep/src/faceDeep.py
+fi
+
+if [ $TENSORBOARD == True ] ; then
+  echo "Starting Tensor Board in Browser ..."
+  /usr/bin/tensorboard --logdir=logs/ &
+  sleep 2s
+  chromium http://localhost:6006/ -incognito
+fi
