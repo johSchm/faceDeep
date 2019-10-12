423acc9b7248b0e5bcaf08ca446a44b114ad558c
johSchm <johSchm@protonmail.com>
2019-10-12 08:06:19 +0200

    Adapted vgg class

--- src/std_models/vgg.py
+++ src/std_models/vgg.py
@@ -29,36 +29,94 @@ from tensorflow.python.keras.models import Model
 from PIL import Image
 from sklearn.metrics import classification_report
 from tabulate import tabulate
+from tqdm import tqdm
+from enum import Enum
+from learner import ImageClassifier
 
 
-VERSIONS = ['16', '19']
+class VGGVersion(Enum):
+    """ Supported VGG verions.
+    """
+    VGG_16 = '16'
+    VGG_19 = '19'
 
 
-class VGGAdapter:
+class VGGAdapter(ImageClassifier):
     """ A VGG Adapter.
     """
 
-    def __init__(self, version='19'):
+    def __init__(self, version=VGGVersion.VGG_19, input_shape=None, output_shape=None):
         """ Initialization method.
+        :param version
+        :param input_shape
+        :param output_shape
         """
-        self.model = self.get_model(version)
+        self.version = version
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.model = self.get_base_model()
+        self.expand_model()
+        self.config_model()
 
-    @staticmethod
-    def get_model(version):
+    def train(self, x_train, y_train, x_val=None, y_val=None,
+              epochs=3, batch_size=32, validation_split=0.3):
+        """ Start training phase.
+        :param x_train:
+        :param y_train:
+        :param x_val:
+        :param y_val:
+        :param epochs: number of epochs
+        :param batch_size
+        :param validation_split
+        """
+        if self.model is not None:
+            x_train = self.normalize(x_train)
+            if x_val is None and y_val is None:
+                self.model.fit(x_train, y_train,
+                               epochs=epochs, batch_size=batch_size,
+                               validation_split=validation_split,
+                               callbacks=self.callbacks)
+            else:
+                self.model.fit(x_train, y_train,
+                               epochs=epochs, batch_size=batch_size,
+                               validation_data=(x_val, y_val),
+                               callbacks=self.callbacks)
+
+    def get_base_model(self):
         """ Returns the corresponding Keras VGG model.
-        :param version:
         :return: model
         """
-        if type(version) is not str:
-            raise TypeError("VGG Version has to be a {}".format(str))
-        if version == '16':
-            return VGG16(include_top=True, weights='imagenet')
-        if version == '19':
-            return VGG19(include_top=True, weights='imagenet')
+        if self.version == VGGVersion.VGG_16:
+            return VGG16(input_shape=self.input_shape, include_top=True, weights='imagenet')
+        if self.version == VGGVersion.VGG_19:
+            return VGG19(input_shape=self.input_shape, include_top=True, weights='imagenet')
         else:
             raise ValueError("VGG Version not found!")
 
-    def get_imagenet_class(self, index):
+    def expand_model(self):
+        """ It is important to freeze the convolutional based
+        before you compile and train the model.
+        By freezing or setting layer.trainable = False,
+        you prevent the weights in a given layer from
+        being updated during training.
+        """
+        self.model.trainable = False
+        global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+        prediction_layer = tf.keras.layers.Dense(len(self.output_shape), activation='softmax')
+        self.model = tf.keras.Sequential([
+            self.model,
+            global_average_layer,
+            prediction_layer])
+
+    def config_model(self):
+        """ Compiles the model.
+        """
+        self.model.compile(optimizer='adam',
+                           loss=tf.keras.losses.sparse_categorical_crossentropy,
+                           metrics=["accuracy"])
+
+    @staticmethod
+    def get_imagenet_class(index):
         """ imagenet_class_index.json:
         https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json
         :param index
@@ -89,13 +147,3 @@ class VGGAdapter:
         ind = pred.argsort()
         return [class_indexes[ind][::-1], pred[ind][::-1] * 100]
 
-    @staticmethod
-    def show_image(img, title):
-        """ Displays a image with title.
-        :param img:
-        :param title:
-        """
-        plt.title("Erkannt : {}\n".format(title))
-        plt.axis('off')
-        plt.imshow(img, interpolation='none')
-        plt.show()
